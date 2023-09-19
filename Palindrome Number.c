ba5efdf66a27d29c58f6e64c1141984b8c9cfe97
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-19 23:06:00 +0530

    Easy Level

--- Palindrome Number.c
+++ Palindrome Number.c
@@ -1,3 +1,4 @@
+// Easy level
 /* =======================================Problem Description ==================================================
 
 Given an integer x, return true if x is a palindrome, and false otherwise.
