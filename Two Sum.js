399967423aeb5e0222f06aca7446388a00d3438a
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-19 23:06:29 +0530

    Easy Level

--- Two Sum.js
+++ Two Sum.js
@@ -1,3 +1,4 @@
+// Easy Level
 /* ============================================ Problem Description ===============================================
 
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
